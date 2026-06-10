00fd5a01271d454bd077bc47b49bca423bdc1816
Hassan Hussien <hakpb7@gmail.com>
2026-06-10 04:49:58 +0300

    add pagination feature

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -35,6 +35,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4. pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const newTours = await Tour.countDocuments();
+      if (skip >= newTours) throw new Error('this tour is not exist');
+    }
+
     // execute the query, filter w query i wrote
     const tours = await query;
 
