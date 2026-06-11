10ea657d031a0717113ca095c09f91ebd4858dcf
Hassan Hussien <hakpb7@gmail.com>
2026-06-11 18:56:09 +0300

    optimize data folder

--- src/data/data/import-dev-data.js
+++ src/data/data/import-dev-data.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: path.join(`${__dirname}../../../../.env` )});
 
-const Tour = require('./../../models/tourModel');
+const Tour = require('../../models/tourModel');
 const DB = process.env.DATABASE;
 
 
