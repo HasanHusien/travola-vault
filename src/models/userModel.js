27d986bc04c680a26406b29dfdbef13a11f6921e
Hassan Hussien <hakpb7@gmail.com>
2026-06-20 17:38:42 +0300

    password hashing crypto and security

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // Inputs: name, email, photo, password, confirmPassword
 const userSchema = new mongoose.Schema({
@@ -22,10 +23,27 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'please provide a password confirm correctly']
+    required: [true, 'please provide a password confirm correctly'],
+
+    // checking if password are the same %% runs before save() and create()
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'passwords are not matched' // err msg
+    }
   }
 });
 
-const UserModel = mongoose.model('User', userSchema);
+userSchema.pre('save', async function(next) {
+  // only execute if password modifying
+  if (!this.isModified('password')) return next();
 
+  // crypt the password by bcrypt package
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+});
+
+const UserModel = mongoose.model('User', userSchema);
 module.exports = UserModel;
