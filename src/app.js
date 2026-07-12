ecc5bacbf54c0e9035ead96aba40dd8ba8ba6eb4
Hassan Hussien <hakpb7@gmail.com>
2026-07-12 20:52:28 +0300

    setting HTTP Headres && install helmet package

--- src/app.js
+++ src/app.js
@@ -3,27 +3,33 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
-const rateLimit = require('express-rate-limit');
 
-// json parser middleware
-app.use(express.json());
+// setting HTTP Headers
+app.use(helmet());
+
+// json parser middleware && setting limit for body data
+app.use(express.json({ limit: '10kb' }));
 
 // morgan middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 // using express-rate-limit package
 // rate limiting algorithm (middleware) for protect from attacks
-const limit = rateLimit({
+const limier = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
   max: 80, // 80 try
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 // to see rate limit look at Headers
-app.use('/api', limit);
+app.use('/api', limiter);
 
 // rotes
 app.use('/api/tours', tourRouter);
