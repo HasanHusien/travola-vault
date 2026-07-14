75e9a5cbfea69a0a28f492c1cdad6d966c4eccc9
Hassan Hussien <hakpb7@gmail.com>
2026-07-14 17:59:18 +0300

    create hpp middleware for sorting security

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -16,6 +17,20 @@ const globalErrorHandler = require('./controllers/errorController');
 
 // setting HTTP Headers
 app.use(helmet());
+
+// add more secure for sorting (using hpp middleware)
+app.use(
+  hpp({
+    // allowed to duplicate these fields names
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'price',
+      'difficulty'
+    ]
+  })
+);
 
 // json parser middleware && setting limit for req.body data
 app.use(express.json({ limit: '10kb' }));
