c84dc5f29be4dd4bf19ecf0410dacdc54c38d03b
Hassan Hussien <hakpb7@gmail.com>
2026-07-17 19:18:27 +0300

    make only auth users can create a review

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getReviews, createReview} =require('../controllers/reviewControllers')
+const { protect, restrictTo } = require('../controllers/authControllers');
+const {
+  getReviews,
+  createReview
+} = require('../controllers/reviewControllers');
 
 router.route('/').get(getReviews);
-router.route('/newReview').post(createReview);
+router.route('/newReview').post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
