a3962de92ac25be2a9b6390c46b0dcc738140bc2
Hassan Hussien <hakpb7@gmail.com>
2026-08-17 17:56:27 +0300

    add more routes for pug process

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-const AppError = require('./utils/appError');
-const express = require('express');
-const morgan = require('morgan');
+const AppError = require("./utils/appError");
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 // security packages
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes')
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
-const globalErrorHandler = require('./controllers/errorController');
+const globalErrorHandler = require("./controllers/errorController");
 
 // setting HTTP Headers
 app.use(helmet());
@@ -24,21 +24,21 @@ app.use(
   hpp({
     // allowed to duplicate these fields names
     whitelist: [
-      'duration',
-      'ratingsAverage',
-      'ratingsQuantity',
-      'price',
-      'difficulty' 
-    ]
-  })
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "price",
+      "difficulty",
+    ],
+  }),
 );
 
 // json parser middleware && setting limit for req.body data
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // morgan middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // data sanitization against NOSQL query injection attacks
@@ -52,22 +52,41 @@ app.use(xss());
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
   max: 80, // 80 try
-  message: 'Too many requests from this IP, Please try again in an hour!'
+  message: "Too many requests from this IP, Please try again in an hour!",
 });
 // to see rate limit look at Headers
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // rotes
-app.use('/api/tours', tourRouter);
-app.use('/api/users', userRouter);
-app.use('/api/reviews',reviewRouter)
+app.get("/", (req, res) => {
+  res.status(200).render("base", {
+    tour: "the forest hiker",
+    user: "hassan",
+  });
+});
+
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "all tours",
+  });
+});
+
+app.get("/tour", (req, res) => {
+  res.status(200).render("tour", {
+    title: "the forest hiker tour",
+  });
+});
+
+app.use("/api/tours", tourRouter);
+app.use("/api/users", userRouter);
+app.use("/api/reviews", reviewRouter);
 
 // all eq all http method & '*' eq all not declared route
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`cannot find ${req.originalUrl} at this server`, 404));
 });
 
-// global error handler 
+// global error handler
 app.use(globalErrorHandler);
 
 module.exports = app;
