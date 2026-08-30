d5d599e99e7ee9fe928699ca930a45a56afe197d
Hassan Hussien <hakpb7@gmail.com>
2026-08-30 19:13:50 +0300

    Delete src directory

--- src/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-// with using this fn no need for try catch block
-// and make don't repeat any code, ann make code
-// readable and more pro, and catch error directly
-
-module.exports = fn => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
