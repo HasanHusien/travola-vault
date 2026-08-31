66ff4ce00a00c20da9ff4def66dbb61bef08dc91
Hassan Hussien <hakpb7@gmail.com>
2026-08-31 19:29:29 +0300

    fix tour styles

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Overview from "./pages/OverView";
 import Tour from "./pages/Tour";
-
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index  element={<Overview />} />
-        <Route path="/tour/:slug" element={<Tour />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Overview />} />
+          <Route path="/tour/:slug" element={<Tour />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- frontEnd/src/pages/AppLayout.jsx
+++ frontEnd/src/pages/AppLayout.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
 function AppLayout() {
   return (
-    <div>
-      
-    </div>
-  )
+    <main>
+      <Header />
+      <Outlet />
+    </main>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
