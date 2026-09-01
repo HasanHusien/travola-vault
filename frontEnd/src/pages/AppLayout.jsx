00ed430f767fdaf0a239022d4d6bfe1296750408
Hassan Hussien <hakpb7@gmail.com>
2026-09-01 04:10:51 +0300

    add the footer to appLoayout

--- frontEnd/src/pages/AppLayout.jsx
+++ frontEnd/src/pages/AppLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 function AppLayout() {
   return (
     <main>
       <Header />
       <Outlet />
+      <Footer />
     </main>
   );
 }
