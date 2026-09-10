0fbe91cfebd0b25da502f709789779ce2a4c7a5e
Hassan Hussien <hakpb7@gmail.com>
2026-09-10 17:50:04 +0300

    update some files

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App.jsx";
 import ErrorFallback from "./components/ErrorFallback.jsx";
 
