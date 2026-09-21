c98e86762daab3c6cb2f505440a10506e2af5a65
Hassan Hussien <hakpb7@gmail.com>
2026-09-21 17:12:35 +0300

    working on email or sending email to user (server)

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -1,17 +1,37 @@
 const nodemailer = require('nodemailer');
 const catchAsync = require('./catchAsync');
 
+// new Email (user,url).sendWelcome()
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Hassan Hussien <${process.env.EMAIL_FORM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // later
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+
+  // sending
+};
+
 const sendEmail = catchAsync(async options => {
   // 1) create transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
 
   // 2) define email options
   const mailOptions = {
@@ -24,5 +44,3 @@ const sendEmail = catchAsync(async options => {
   // 3) sent the email
   await transporter.sendMail(mailOptions);
 });
-
-module.exports = sendEmail;
